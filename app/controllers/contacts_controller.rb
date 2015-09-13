675385b25ee50b4f1e5571fcc16fb1db46f2e947
harshad-abdulla <harshad.adll@gmail.com>
2015-09-13 16:10:32 +0530

    Added flash and database save

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,13 @@ class ContactsController < ApplicationController
 	end
 
 	def create
-		
+		@contact = Contact.new(contact.params)
+		if contact.save
+			flash[:success]="Message Sent."
+			redirect_to new_contact_path
+		else
+			flash[:danger]="Error occure, message not sent!"
+			redirect_to new_contact_path
 	end
 
 end
