4c4ebd4dc5d7e699c0f6ce4d092d8bfa32f263be
harshad-abdulla <harshad.adll@gmail.com>
2015-09-13 16:14:40 +0530

    Added security in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,23 @@
 class ContactsController < ApplicationController
 
 	def new
-		@contact = Contact.new
+		@contact =Contact.new
+		
 	end
 
 	def create
-		@contact = Contact.new(contact.params)
-		if contact.save
-			flash[:success]="Message Sent."
+		@contact =Contact.new(contact_params)
+		if @contact.save
+			flash[:success] = "Message sent."
 			redirect_to new_contact_path
 		else
-			flash[:danger]="Error occure, message not sent!"
-			redirect_to new_contact_path
+			flash[:danger] = "Error occured, Message not sent!"
+			redirect_to new_contact_path			
+		end
 	end
 
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end
 end
