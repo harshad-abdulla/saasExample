f3c42b9f805c4fdd0ebd218bcbb0475e0ecac32d
harshad-abdulla <harshad.adll@gmail.com>
2015-09-13 15:59:04 +0530

    Added ne migration, modified contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906162509) do
+ActiveRecord::Schema.define(version: 20150913101737) do
 
   create_table "contacts", force: true do |t|
+    t.string "name"
+    t.string "email"
+    t.text   "comments"
   end
 
 end
